5f59a12122026e98853dc789422b23b5b55ba502
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-07-18 20:45:53 -0700

    build: adding task 5 nd update header

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - checks if node is root
+ * @node: pointer to the node to check
+ * Return: 1 if root and 0 if not
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if ((node) && (node->parent == NULL))
+		return (1);
+	return (0);
+}
