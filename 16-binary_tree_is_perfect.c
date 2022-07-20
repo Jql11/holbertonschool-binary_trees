31b6c0559e75a71e75edf5c7478bc4d7dffc753f
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-07-19 18:18:26 -0700

    Fix: betty style error

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -75,7 +75,9 @@ int binary_tree_is_full(const binary_tree_t *tree)
 }
 
 /**
- *
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: pointer to the root node of the tree to check
+ * Return: 1 if perfect or 0 if not
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
