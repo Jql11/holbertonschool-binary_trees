bd8f3b86d5df09de07c6c4f5e2846bc274dc463d
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-07-20 21:21:17 -0700

    fix: bitwise comarison variable

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -80,7 +80,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int is_tree_balanced = (binary_tree_balance(tree) == 0) ? 1 : 0;
+	char is_tree_balanced = (binary_tree_balance(tree) == 0) ? 1 : 0;
 	int is_tree_full = binary_tree_is_full(tree);
 
 	if (!tree)
