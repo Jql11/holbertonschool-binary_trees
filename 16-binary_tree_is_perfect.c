5eb68298b7fa1bd9af868fbd729a7d506806fa9e
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-07-20 20:33:45 -0700

    fix: added early return

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -87,6 +87,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
 	is_tree_balanced = (binary_tree_balance(tree) == 0) ? 1 : 0;
 	is_tree_full = binary_tree_is_full(tree);
 
