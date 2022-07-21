9aa8f7748aa4d1c76f01642aef6462c1c159ae84
Jql11 <4325@holbertonschool.com>
2022-07-21 18:35:59 +1000

    change back to what it was

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -80,11 +80,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (!tree || binary_tree_balance(tree) != 0)
+	int is_tree_balanced = (binary_tree_balance(tree) == 0) ? 1 : 0;
+	int is_tree_full = binary_tree_is_full(tree);
+
+
+	if (!tree)
 		return (0);
-	if (binary_tree_balance(tree) == 0 && binary_tree_is_full(tree) == 1)
-		return (1);
-	if (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
-		return (1);
-	return (0);
+	return (is_tree_balanced & is_tree_full);
 }
